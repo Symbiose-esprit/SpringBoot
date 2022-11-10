66d0dbaa97d96c0bc22236f55a7089b821f19219
yesminebaoueb <yasmine.baouab@esprit.tn>
2022-11-10 17:37:01 +0100

    .

--- src/test/java/com/esprit/examen/services/OperateurServiceTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServiceTest.java
@@ -6,20 +6,45 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.esprit.examen.entities.Operateur;
+import com.esprit.examen.repositories.OperateurRepository;
 
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 public class OperateurServiceTest {
 
 	@Autowired
 	IOperateurService operateurService;
+	 @Autowired
+	    OperateurRepository operateurRepository;
 	
+
+	@BeforeEach
+	void
+	setUp() {
+		operateurRepository.deleteAll();
+	}
+
+	@AfterEach
+	void
+	setDown() {
+		operateurRepository.deleteAll();
+	}
 	//testing Add method
 	@Test
+	@Order(2)
 	public void testAddOperateur(){
 		List<Operateur> operateurs = operateurService.retrieveAllOperateurs();
 		int expected = operateurs.size();
@@ -36,6 +61,7 @@ public class OperateurServiceTest {
 	
 	//Testing retrieveOperateur
 	@Test
+	@Order(1)
 	public void testRetrieveOperateurs() {
 		Operateur o = new Operateur();
 		o.setNom("yasss");
@@ -67,6 +93,7 @@ public class OperateurServiceTest {
 	
 	//Testing deleteOperateur
 	@Test
+	@Order(3)
 	public void testDeleteOperateur() {
 		Operateur o = new Operateur();
 		o.setNom("hichem");
