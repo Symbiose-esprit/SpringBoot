85c63dea4225503fb519b4306674be413ab8b477
oussema-mestiri2 <oussema.mestiri@esprit.tn>
2022-11-10 23:02:14 +0100

    docker-compose built

--- src/test/java/com/esprit/examen/TpAchatProjectApplicationTests.java
+++ src/test/java/com/esprit/examen/TpAchatProjectApplicationTests.java
@@ -1,4 +1,4 @@
-/* package com.esprit.examen;
+package com.esprit.examen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class TpAchatProjectApplicationTests {
 	void contextLoads() {
 	}
 
-} */
+}
---
+++ src/test/java/com/esprit/examen/services/OperateurServiceTest.java
@@ -0,0 +1,144 @@
+package com.esprit.examen.services;
+
+
+import com.esprit.examen.entities.*;
+import com.esprit.examen.repositories.FactureRepository;
+import com.esprit.examen.repositories.OperateurRepository;
+import lombok.var;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+
+import static org.junit.Assert.assertNull;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class OperateurServiceTest {
+
+    @Autowired
+    OperateurRepository operateurRepository;
+
+    @Autowired
+    FactureRepository factureRepository;
+    @Autowired
+    OperateurServiceImpl operateurService;
+    @AfterEach
+    void
+    setDown() {
+        operateurRepository.deleteAll();
+    }
+    @BeforeEach
+    void
+    setUp() {
+        operateurRepository.deleteAll();
+    }
+    @Test
+    public void retrieveAllOperateurs(){
+        var facture = new Facture();
+        facture.setMontantRemise(0.5F);
+        facture.setArchivee(true);
+        // facture.setArchivee(false);
+
+
+        facture.setMontantFacture(10F);
+        facture.setDateCreationFacture(new Date());
+
+        facture.setDateDerniereModificationFacture(new Date());
+        factureRepository.save(facture);
+        var listFactures = new HashSet<Facture>();
+        listFactures.add(facture);
+        // operateur.setFactures(listFactures);
+
+        var operateur = new Operateur();
+        operateur.setFactures(listFactures);
+        operateur.setNom("hello");
+		
+        operateur.setPrenom("hello");
+        operateur.setPassword("admin");/* 
+        var rs = operateurService.retrieveAllOperateurs();
+        Assertions.assertEquals(1 , rs.size());
+        Assertions.assertEquals("admin",rs.get(0).getNom()); */
+        operateurRepository.save(operateur);
+        var rs = operateurService.retrieveAllOperateurs();
+        Assertions.assertEquals(1 , rs.size());
+        Assertions.assertEquals("admin",rs.get(0).getNom());
+        Assertions.assertNotNull(operateur.getFactures());
+    }
+
+    @Test
+    public void addOperateur(){
+        var operateur = new Operateur(1L,"hello","hello","admin",new HashSet<Facture>());
+
+        var rs = operateurService.addOperateur(operateur);
+        Assertions.assertEquals("hello",rs.getNom());
+/* 
+		var operateur = new Operateur(1L,"hello","hello","admin",new HashSet<Facture>());
+        var rs = operateurService.addOperateur(operateur);
+        Assertions.assertEquals("hello",rs.getNom()); */
+        Assertions.assertEquals("hello",rs.getPrenom());
+
+        Assertions.assertEquals("admin",rs.getPassword());
+
+    }
+    @Test
+    public void deleteOperateur(){
+        var operateur = new Operateur();
+        operateur.setNom("hello");
+
+        operateur.setPrenom("hello");
+        
+		operateur.setPassword("admin");
+        var rs = operateurRepository.save(operateur);
+        operateurService.deleteOperateur(rs.getIdOperateur());
+		        // assertNull(operateurService.retrieveOperateur(rs.getIdOperateur()));
+
+        assertNull(operateurService.retrieveOperateur(rs.getIdOperateur()));
+    }
+/*     @Test
+    public void deleteOperateur(){
+        var operateur = new Operateur();
+        operateur.setNom("hello");
+        operateur.setPrenom("hello");
+        operateur.setPassword("admin");
+        var rs = operateurRepository.save(operateur);
+
+        operateurService.deleteOperateur(rs.getIdOperateur());
+        assertNull(operateurService.retrieveOperateur(rs.getIdOperateur()));
+    } */
+
+    @Test
+    public void retrieveOperateur(){
+        var operateur = new Operateur();
+        operateur.setNom("hello");
+        operateur.setPrenom("hello");
+
+        operateur.setPassword("admin");
+			
+/*         var op = operateurRepository.save(operateur);
+
+        var rs = operateurService.retrieveOperateur(op.getIdOperateur());
+        Assertions.assertEquals(op.getIdOperateur(),rs.getIdOperateur()); */
+        var op = operateurRepository.save(operateur);
+
+        var rs = operateurService.retrieveOperateur(op.getIdOperateur());
+        Assertions.assertEquals(op.getIdOperateur(),rs.getIdOperateur());
+        Assertions.assertEquals(op.getNom(),rs.getNom());
+		//      operateur.setNom("hello");
+        // operateur.setPrenom("hello");
+
+        // operateur.setPassword("admin");
+		
+    }
+}
