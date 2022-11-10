40270ba8298899345662468ada4093e03c8f780a
oussema-mestiri2 <oussema.mestiri@esprit.tn>
2022-11-10 15:11:14 +0100

    Update UserServiceImplTest.java

--- src/test/java/com/esprit/examen/services/UserServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/UserServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.esprit.examen.services;
+/* package com.esprit.examen.services;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
@@ -59,4 +59,4 @@ public class UserServiceImplTest {
 		verify(repository, times(1)).delete(user);
 	}
 
-}
+} */
