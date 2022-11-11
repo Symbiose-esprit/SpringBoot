838a613ec3efe6cb2446169780a1028f58213891
El Behi Mahdi <mahdi.behi@esprit.tn>
2022-11-11 08:54:58 +0100

    Update SecteurActivTest.java

--- src/test/java/com/esprit/examen/services/SecteurActivTest.java
+++ src/test/java/com/esprit/examen/services/SecteurActivTest.java
@@ -22,8 +22,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SecteurActivTest {
 
     @Autowired
@@ -32,7 +32,7 @@ public class SecteurActivTest {
     @MockBean
     private SecteurActiviteRepository repository;
 
-    /*
+    
     @Test
     public List<SecteurActivite> getAllSecteurActivite()
     {
